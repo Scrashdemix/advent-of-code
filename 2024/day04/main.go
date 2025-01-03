85ba90940ad10e58c4e0dd0361e04e1f31f8cb26
Alexander Maisch <alexander.maisch@web.de>
2025-01-03 17:07:42 +0100

    Add solution for day 04, part 2

--- 2024/day04/main.go
+++ 2024/day04/main.go
@@ -8,22 +8,11 @@ import (
 	"os"
 )
 
-var directions [][]int = [][]int {
-	{1, 0}, // down
-	{0, 1}, // right
-	{-1, 0}, // up
-	{0, -1}, // left
-	{1, 1}, // down-right
-	{-1, -1}, // up-left
-	{-1, 1}, // up-right
-	{1, -1}, // down-left
-}
-
 func main()  {
 	fileFlag := flag.String("input", "input.txt", "Name of the file containing the text")
 	// textField [idxCol][idxRow]
 	var textField [][]string = readFile(*fileFlag)
-	var count int = countString(textField, "XMAS")
+	var count int = countString(textField)
 	fmt.Println(count)
 }
 
@@ -46,37 +35,35 @@ func readFile(fileName string) [][]string {
 	return result
 }
 
-func countString(textField [][]string, searchString string) int {
+func countString(textField [][]string) int {
 	var count int = 0
-	for i := 0; i < len(textField); i++ {
-		for j := 0; j < len(textField[i]); j++ {
-			for _, dir := range directions {
-				resultingString := getStringFromField(textField, i, j, dir, len(searchString))
-				if resultingString == searchString {
-					count += 1
-				}
+	for i := 1; i < len(textField)-1; i++ {
+		for j := 1; j < len(textField[i])-1; j++ {
+			if checkForCrossString(textField, i, j) {
+				count += 1
 			}
 		}
 	}
 	return count
 }
 
-func getStringFromField(
+func checkForCrossString(
 	textField [][]string,
 	idxCol int,
-	idxRow int,
-	direction []int,
-	length int) string {
-		if (direction[0] == -1 && idxCol < length-1) ||
-		(direction[0] == 1 && (len(textField)-idxCol) <= length-1) ||
-		(direction[1] == -1 && idxRow < length-1) ||
-		(direction[1] == 1 && (len(textField[idxCol])-idxRow) <= length-1) {
-			return ""
-		}
+	idxRow int) bool {
+	if textField[idxCol][idxRow] != "A" {
+		return false
+	}
+	if isMAS(textField[idxCol-1][idxRow-1], textField[idxCol+1][idxRow+1]) &&
+		isMAS(textField[idxCol-1][idxRow+1], textField[idxCol+1][idxRow-1]) {
+		return true
+	}
+	return false
+}
 
-		var tempString = ""
-		for i := 0; i < length; i++ {
-			tempString += textField[idxCol + i*direction[0]][idxRow + i*direction[1]]
-		}
-		return tempString
+func isMAS(topCorner string, bottomCorner string) bool {
+	if (topCorner == "M" && bottomCorner == "S") || (topCorner == "S" && bottomCorner == "M") {
+		return true
+	}
+	return false
 }
